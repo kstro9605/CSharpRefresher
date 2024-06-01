b12133a4357a5f7b1dc438cebebf2316dde083d3
kstro9605 <110832013+kstro9605@users.noreply.github.com>
2024-06-01 15:52:26 -0600

    Commit of Project using Visual Studio and not dotnet for unitTesting practice (#3)
    Approved for main branch merge

---
+++ SimpleProjects/VS Studio/SimpleCalculator/InputValidation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    public class InputValidation
+    {
+        public double ValidNumericalInputRetry(string input)
+        {
+            double num;
+            if (!double.TryParse(input, out num))
+            {
+                while (true)
+                {
+                    Console.WriteLine("Not a valid numerical type please retry.");
+                    if (double.TryParse(Console.ReadLine(), out num))
+                    {
+                        return num;
+                    }
+                }
+            }
+            else
+            {
+                return num;
+            }
+        }
+        public double ValidNumericalInput(string input)
+        {
+            double num;
+            if (!double.TryParse(input, out num))
+            {
+                throw new ArgumentException("Expected a Numerical Value");
+            }
+            return num; 
+        }
+    }
+}
---
+++ SimpleProjects/VS Studio/SimpleCalculator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    public class SimpleCalculator
+    {
+        static void Main(string[] args)
+        {
+            InputValidation ipVal = new InputValidation();
+            CalculatorEngine engine = new CalculatorEngine();  
+
+            Console.WriteLine("Hello! Please enter the first number for calculation");
+            double a = ipVal.ValidNumericalInput(Console.ReadLine()); 
+
+            Console.WriteLine("Please enter the second number for calculation");
+            double b = ipVal.ValidNumericalInput(Console.ReadLine());
+
+            Console.WriteLine("Please enter the operation for calculation");
+            string operation = Console.ReadLine();
+
+            engine.Calculate(a, b, operation);
+
+            Console.ReadLine();
+        }
+
+    }
+}
