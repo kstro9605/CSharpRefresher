573b8e1be362004c8a83d6ad10bea46d8a5229ed
kstro9605 <110832013+kstro9605@users.noreply.github.com>
2024-12-10 05:51:26 +0000

    Cleanup unneeded projects and commit test on ios device via vscode online

---
+++ SimpleProjects/VS Studio/ipadDev/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ipadDev
+{
+    public class testClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+            Console.ReadLine();
+        }
+    }
+
+}
